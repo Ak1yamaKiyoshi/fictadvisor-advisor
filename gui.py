7e05f8fd41180838a36ec72420925a7ea5096fa5
Kolosov Ihor <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2024-02-14 02:01:25 +0200

    minor changes

--- gui.py
+++ gui.py
@@ -2,10 +2,11 @@ from run import MistralChatWrapper, WrapperLLM, mistral_api_key, FileScrapper, s
 import sys
 import os
 import re
+import colorama
 from datetime import datetime
 from PyQt5.QtCore import QEvent, Qt
 from PyQt5.QtGui import QFont, QTextCursor
-from PyQt5.QtWidgets import QApplication, QDialog,QLabel, QFileDialog, QComboBox, QHBoxLayout, QWidget, QVBoxLayout, QPushButton, QTextEdit, QScrollArea, QMessageBox
+from PyQt5.QtWidgets import QApplication, QDialog,QLabel, QLineEdit, QFileDialog, QComboBox, QHBoxLayout, QWidget, QVBoxLayout, QPushButton, QTextEdit, QScrollArea, QMessageBox
 from PyQt5.QtCore import QTimer
 
 from typing import List, Optional, Tuple
@@ -18,10 +19,13 @@ class ChatApp(QWidget):
         super().__init__()
         self.llm:WrapperLLM = None
         self.embeddings_paths:List[str] = list()
+        self.embeddings_queries:List[str] = list()
         self.current_conversation_name = None
 
+        self.line_edits = None
+
         self.avaivable_models = ['mistral-tiny', 'mistral-small', 'mistral-medium']
-        self.font_size:int = 17
+        self.font_size:int = 12
         self.max_tokens:int = 21000
 
         self.break_line_size = 100
@@ -29,6 +33,7 @@ class ChatApp(QWidget):
         self.default_model = self.avaivable_models[0]
         self.is_start_of_conversation = True
 
+        self.first_init = True
         self.init_model()
         self.init_ui()
 
@@ -43,7 +48,6 @@ class ChatApp(QWidget):
             return OpenAIChatWrapper
 
     def model_name_changed(self, model:str):
-
         #self.llm.llm = self.get_model_by_name(model)
         self.llm.llm.model = model
 
@@ -79,7 +83,7 @@ class ChatApp(QWidget):
         model.setup()
         name = model.invoke("Write a perfect name for conversation that starts like this: " + str(self.llm.history[1]['content']))
         current_time = datetime.now().strftime("%H:%M")
-        self.current_conversation_name = f'./conversations/[{current_time}]_{name.replace(" ", "_").lower()}.txt'
+        self.current_conversation_name = f'./conversations/[{current_time}]_{name.replace(" ", "_").lower()[:20]}.txt'
 
     def cache_conversation(self):
         self.cache_system_prompt()
@@ -93,11 +97,10 @@ class ChatApp(QWidget):
         system_prompt = self.system_prompt_editor.toPlainText()
         user_message = self.user_input.toPlainText()
 
-        #if "SRCH_EMBD" in user_message:
-        #    user_message += "Embeddings: " +self.get_embeddings(user_message)
-        #    user_message = user_message.replace("SRCH_EMBD", "")
+        if "SRCH_EMBD" in user_message:
+            user_message += "\nEmbeddings: " + self.get_embeddings()
+            user_message = user_message.replace("SRCH_EMBD", "")
         self.user_input.clear()
-        print(system_prompt)
         self.llm.set_system_prompt(system_prompt if system_prompt else "You are MistralAI")
 
         self.llm.invoke(user_message)
@@ -106,7 +109,35 @@ class ChatApp(QWidget):
         if self.is_start_of_conversation:
             self.is_start_of_conversation = False
             self.init_conversation()
-        #self.blink_green()
+
+    def get_embeddings(self):
+        if not self.embeddings_paths:
+            return
+
+        for path, query in zip(self.embeddings_paths, self.embeddings_queries):
+            file_content = FileScrapper().scrap_file(path)
+            print(f" [ -- ] File content lenght: {len(file_content)}")
+            file_content = split_text_into_chunks(file_content, 2*256)
+            file_embeddings = self.llm.get_embeddings(file_content)
+            search = self.llm.embedding_search(
+                file_content, file_embeddings, query
+            )
+            return "\n ".join(search)
+
+    def break_line(self, content):
+        new_content = ""
+        code_flag = False
+        for line in content.split("\n"):
+            if not code_flag and '```' in line:
+                code_flag = True
+            elif '```' in line:
+                code_flag = False
+            if code_flag:
+                new_content += line + "\n"
+                continue
+            n = (180-self.font_size*10)
+            new_content += '\n'.join([line[i:i+n ] for i in range(0, len(line), n)]) + "\n"
+        return new_content
 
     def update_chat_history(self):
         self.chat_log.clear()
@@ -115,26 +146,33 @@ class ChatApp(QWidget):
         for entry in self.llm.history:
             role = entry["role"]
             content = entry["content"]
+            content = self.break_line(content)
             #content = self.format_code(content)
             if role.lower() == 'user':
                 html_content += self.__user_message_wrapper_html(content)
             elif role.lower() == 'system':
                 html_content += self.__system_message_wrapper_html(content)
             elif role.lower() == 'assistant':
-                html_content += self.__assistant_message_wrapper_html(content)
+                if entry == self.llm.history[-1]:
+                    html_content += self.__last_message(content)
+                else:
+                    html_content += self.__assistant_message_wrapper_html(content)
             html_content += '<br>'
         html_content += '</div>'
 
         self.chat_log.setHtml(html_content)
+        self.chat_log.moveCursor(QTextCursor.End)
         if not self.is_start_of_conversation:
             self.cache_conversation()
 
     def __user_message_wrapper_html(self, content):
-        return f"""<b style="color:rgb(200, 180, 0);font-weight:1200;">User:</b><br><code>{content}</code>"""
+        return f"""<b style="color:rgb(200, 180, 0);font-weight:1200;">User:</b><br><code style="color:rgb(180, 180, 180);">{content}</code>"""
     def __assistant_message_wrapper_html(self, content):
-        return f"""<b style="color:rgb(58, 0, 214);font-weight:1200;">Assistant:</b><br><code>{content}</code>"""
+        return f"""<b style="color:rgb(58, 20, 184);font-weight:1200;">Assistant:</b><br><code style="color:rgb(180, 180, 180);">{content}</code>"""
     def __system_message_wrapper_html(self, content):
         return f'{content}'
+    def __last_message(self, content):
+        return f"""<b style="color:rgb(58, 0, 214);font-weight:1200;">Assistant:</b><br><code style="color:rgb(255, 255, 255);">{content}</code>"""
 
     def eventFilter(self, source, event):
         if event.type() == QEvent.KeyPress:
@@ -148,7 +186,7 @@ class ChatApp(QWidget):
 
     def init_ui(self):
         self.setWindowTitle("Mistral Chat")
-        self.setGeometry(100, 100, 400, 600)
+        self.setGeometry(100, 100, 470, 600)
         self.setWindowFlags(Qt.Window | Qt.WindowMinimizeButtonHint | Qt.WindowMaximizeButtonHint | Qt.WindowStaysOnTopHint)
 
         # Layouts
@@ -169,7 +207,7 @@ class ChatApp(QWidget):
 
         # System prompt
         self.system_prompt_editor = QTextEdit()
-        self.system_prompt_editor.setPlaceholderText(" Your System Prompt here. ")
+        self.system_prompt_editor.setPlaceholderText("Your System Prompt here. ")
         self.system_prompt_editor.font = QFont('Monospace')
         self.system_prompt_editor.setFixedHeight(70)
         prompt_layout.addWidget(self.system_prompt_editor)
@@ -177,7 +215,7 @@ class ChatApp(QWidget):
 
         # User input
         self.user_input = QTextEdit()
-        self.user_input.setPlaceholderText(" Your Query here. \n include SRCH_EMBD to append found embeddings, if files to embed added.  ")
+        self.user_input.setPlaceholderText("Your Query here. \nInclude SRCH_EMBD into your query to append chunks of text that found in embeddings, if files to embed added.  ")
         self.user_input.font = QFont('Monospace')
         self.user_input.installEventFilter(self)
         self.user_input.setFixedHeight(140)
@@ -211,24 +249,7 @@ class ChatApp(QWidget):
         self.add_embedding_button.setFixedSize(25, 25)
         button_layout.addWidget(self.add_embedding_button)
 
-        # Add embedings modal
-        self.add_embedding_box = QDialog(self)
-        self.add_embedding_button.clicked.connect(self.add_embedding_box.exec_)
-
-        self.embeddings_list = QTextEdit()
-        if self.embeddings_paths != None and len(self.embeddings_paths) > 0:
-            self.embeddings_list.setText("[" + ", ".join([f'"{path}",' for path in self.embeddings_paths]))
-        embedding_box_layout = QVBoxLayout()
-        self.add_embedding_box.setLayout(embedding_box_layout)
-        embedding_box_layout.addWidget(QLabel(text="To remove path, erase line with that path.\n To save, press submit button"))
-        embedding_box_layout.addWidget(self.embeddings_list)
-        embedding_box_add_path_button = QPushButton("Add path")
-        embedding_box_add_path_button.clicked.connect(self.handle_add_file_path_to_embedding_file_paths)
-        embedding_box_layout.addWidget(embedding_box_add_path_button)
-        embedding_box_layout_save_paths = QPushButton("Submit")
-        embedding_box_layout_save_paths.clicked.connect(self.handle_text_edit_at_embedding_box)
-        embedding_box_layout.addWidget(embedding_box_layout_save_paths)
-
+        self.draw_embeddings()
         # Reload button
         self.reload_button = QPushButton("R")
         self.reload_button.clicked.connect(self.update_chat_history)
@@ -258,26 +279,72 @@ class ChatApp(QWidget):
         self.llm.set_history(self.llm.history[:-1])
         self.update_chat_history()
 
+    def draw_embeddings(self, display=True):
+        # Add embedings modal
+        try:
+            self.add_embedding_box.close()
+        except:
+            pass
+        self.add_embedding_box = QDialog(self)
+        self.add_embedding_button.clicked.connect(self.add_embedding_box.exec_)
+
+        self.embedding_box_layout = None
+        self.embedding_box_layout = QVBoxLayout()
+        self.add_embedding_box.setLayout(self.embedding_box_layout)
+
+        self.embeddings_list = QTextEdit()
+        self.button_list = []
+        self.line_edits = []
+        if self.embeddings_paths != None and len(self.embeddings_paths) > 0:
+            for path in self.embeddings_paths:
+                label = QLabel(path)
+                button = QPushButton("X")
+                button.setFixedSize(25, 25)
+                self.button_list.append(button)
+                button.clicked.connect(lambda: self.embeddings_paths.remove(path) or self.draw_embeddings(True))
+                layout = QHBoxLayout()
+                Vlayout = QVBoxLayout()
+                layout.addLayout(Vlayout)
+                Vlayout.addWidget(label)
+                line_edit = QLineEdit()
+                line_edit.setPlaceholderText("Query for embedded search: ")
+                self.line_edits.append(line_edit)
+                Vlayout.addWidget(line_edit)
+                layout.addWidget(button)
+                self.embedding_box_layout.addLayout(layout)
+
+        embedding_box_add_path_button = QPushButton("Add path")
+        embedding_box_add_path_button.clicked.connect(self.handle_add_file_path_to_embedding_file_paths)
+        self.embedding_box_layout.addWidget(embedding_box_add_path_button)
+        embedding_box_layout_save_paths = QPushButton("Submit")
+        embedding_box_layout_save_paths.clicked.connect(self.handle_text_edit_at_embedding_box)
+        self.embedding_box_layout.addWidget(embedding_box_layout_save_paths)
+
+        self.add_embedding_box.exec_()
+
     def handle_add_file_path_to_embedding_file_paths(self):
         from colorama import Fore, init, Style
-        self.embeddings_paths += self.open_file_dialog()
+        self.open_file_dialog()
         print(f"{Style.BRIGHT}{Fore.GREEN} Embeddings: {self.embeddings_paths} {Style.RESET_ALL}")
-        if self.embeddings_paths != None and len(self.embeddings_paths) > 0:
-            self.embeddings_list.setText("\n".join( self.embeddings_paths))
+        self.add_embedding_box.close()
+        self.draw_embeddings(True)
 
     def open_file_dialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         file_names, _ = QFileDialog.getOpenFileNames(self, "Select Files", "", "All Files (*);;Text Files (*.txt)", options=options)
+        self.embeddings_paths += file_names
         return file_names
 
     def handle_text_edit_at_embedding_box(self):
         from colorama import Fore, init, Style
-        self.embeddings_paths =[
-            path for path in self.embeddings_list.toPlainText().split("\n") if path.strip() != ""
-        ]
-        print(f"{Style.BRIGHT}{Fore.YELLOW} Embeddings: {self.embeddings_paths} {Style.RESET_ALL}")
+        self.embeddings_queries= list()
 
+        if self.line_edits:
+            for i, edit in enumerate(self.line_edits):
+                self.embeddings_queries.append(edit.text())
+        print(f"{Style.BRIGHT}{Fore.YELLOW} Embeddings: {self.embeddings_paths}\n Queries: {self.embeddings_queries} {Style.RESET_ALL}")
+        self.add_embedding_box.close()
 
 
 
@@ -548,14 +615,6 @@ class MistralChatApp(QWidget):
                 self.close()
                 return True
         return super().eventFilter(source, event)
-
-
-
-
-
-
-
-
 
 
 
